544bf57e125f854fa523c470d15922152f63c0e2
natertater23 <angelbaseball2@gmail.com>
2019-10-16 15:09:11 -0700

    goomba

--- goomba.py
+++ goomba.py
@@ -8,6 +8,11 @@ class Goomba(Sprite):
         self. screen = screen
         self.settings = settings
 
+        self.speed = 5
+        self.direction = range(0,2)
+        self.is_alive = True
+
+
         self.image = GOOMBA_SPRITES['0']
         self.index = 0
         self.rect = self.image.get_rect()
@@ -17,5 +22,21 @@ class Goomba(Sprite):
 
         self.x = float(self.rect.x)
 
+
+
         def blitme(self):
             self.screen.blit(self.image, self.rect)
+
+        def upate(self):
+            loop = True
+            # Move left
+            while loop:
+                if self.direction == 0:
+                    self.rect.x -= self.speed
+                else:
+                    self.rect.x += self.speed
+
+                self.direction = range(0, 2)
+                if not self.is_alive:
+                    loop = False
+
